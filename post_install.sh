032d53aa337595f5b96b565c4a0da5801fdf81a4
Josh Worden <josh.worden@cygnus.com>
2014-08-19 23:18:02 -0500

    Updated script to enable installed pecl modules for php

--- post_install.sh
+++ post_install.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
 echo 'POST-INSTALL: Executing Post-Installation Tasks (Say y to everything)'
+
 echo 'POST-INSTALL: Loading Webtatic Repo'
 sudo rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
+
 echo 'POST-INSTALL: Loading RPMForge Repo'
 sudo rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+
 echo 'POST-INSTALL: Installing Packages'
 sudo yum install --enablerepo=webtatic-testing php56w php65w-pdo php56w-pear php56w-opcache php56w-common php56w-devel git htop gcc -y
+sudo chkconfig httpd on
+
 echo 'POST-INSTALL: Downloading Composer'
 sudo curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
+
 echo 'POST-INSTALL: PECL Extensions'
 yes '' | sudo pecl install mongo redis xdebug
-sudo chkconfig httpd on
+echo "extension=mongo.so" > /etc/php.d/mongo.ini
+echo "extension=redis.so" > /etc/php.d/redis.ini
+echo "zend_extension=xdebug.so" > /etc/php.d/xdebug.ini
